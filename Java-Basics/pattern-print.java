7ae8a589a84aa884a1e4c30b6faa7ad0327214c1
Aryan Sharma <149099912+aryan0955@users.noreply.github.com>
2023-12-30 19:39:01 +0530

    Create pattern-print.java

---
+++ Java-Basics/pattern-print.java
@@ -0,0 +1,10 @@
+public class pattern-print{
+  public static void main(String args[]){
+    System.out.print("Patter Print Programe \n\n");
+    System.out.println("*****");
+    System.out.println("****");
+    System.out.println("***");
+    System.out.println("**");
+    System.out.println("*");
+  }
+}
