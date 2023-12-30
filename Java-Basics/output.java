da3abf732e98a800cccf5e2a55ae318c59973c63
Aryan Sharma <149099912+aryan0955@users.noreply.github.com>
2023-12-30 19:10:49 +0530

    Create output.java

---
+++ Java-Basics/output.java
@@ -0,0 +1,7 @@
+public class output{
+    public static void main(String args[]){
+        System.out.print("Hello World\n"); //here \n is used to create space before executing next line 
+        System.out.println("Hello World");  //here ln after print word do same thing as \n
+        System.out.print("Hello World");
+    }
+}
