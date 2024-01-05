7fcc5e3ce7e1b339b35781ec53b551c06cd7d8ca
Aryan Sharma <149099912+aryan0955@users.noreply.github.com>
2024-01-05 19:21:13 +0530

    Update and rename area-of-circle.java to AreaOfCircle.java

--- Java-Basics/AreaOfCircle.java
+++ Java-Basics/AreaOfCircle.java
@@ -1,6 +1,6 @@
 import java.utils.*;
 
-public class area-of-circle{
+public class AreaOfCircle{
   public static void main(String args[]){
   Scanner sc = new Scanner(System.in);
   double radius = sc.nextInt();
